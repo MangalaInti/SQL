0559052dbba7be0ff6ef58cd898076c9c5d1d41d
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2024-05-09 14:32:05 -0400

    Update Sql_Rules.sql

--- Sql_Rules.sql
+++ Sql_Rules.sql
@@ -1,2 +1,4 @@
 # Wherever you are given a range, keep MIN() and MAX() in mind
 # When you want to find first_login use min function
+# Convert rows to columns use case statement
+# Convert column to rows use union e.g - Rearrange Products Table
