2d4e48482253a77d020ff0f2263776d100c63d17
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2024-07-16 14:18:25 -0400

    Update Day10.sql

--- BossCode_Academy/Day10.sql
+++ BossCode_Academy/Day10.sql
@@ -1,4 +1,36 @@
 --DAY10
+--Example Question
+1. WITH RECURSIVE subordinates AS (
+    -- Base case: Select the top-level managers (those who have no manager)
+    SELECT
+        id,
+        name,
+        manager_id,
+        name AS top_manager_name
+    FROM employees
+    WHERE manager_id IS NULL
+
+    UNION ALL
+
+    -- Recursive case: Select the subordinates and their top manager
+    SELECT
+        e.id,
+        e.name,
+        e.manager_id,
+        s.top_manager_name
+    FROM employees e
+    INNER JOIN subordinates s ON e.manager_id = s.id
+)
+
+
+SELECT
+    id,
+    name,
+    manager_id,
+    top_manager_name
+FROM subordinates;
+
+
 1. WITH RECURSIVE subcategories AS (
     -- Anchor member: Select the top-level categories (those without a parent)
     SELECT 
